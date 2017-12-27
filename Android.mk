9d7daa0a8e75786b90480ca0c5dfc6b549b4a456
doc HD <doc.divxm@gmail.com>
2017-12-27 13:47:57 +0300

    tinicompress: Add back libtinycompress_vendor
    Change-Id: I5d01c380106c38dccdb0903e71f5c2a2a4882ac0

--- Android.mk
+++ Android.mk
@@ -10,6 +10,7 @@ LOCAL_CFLAGS := -Wno-macro-redefined
 LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_CFLAGS += -DUSE_VENDOR_EXTN
 LOCAL_SRC_FILES:= compress.c utils.c
 LOCAL_MODULE := libtinycompress_vendor
 LOCAL_SHARED_LIBRARIES:= libcutils libutils
@@ -35,8 +36,7 @@ LOCAL_CFLAGS := -Wno-macro-redefined
 LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include
 LOCAL_SRC_FILES:= cplay.c
 LOCAL_MODULE := cplay
-LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinycompress
+LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinycompress_vendor
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
-
